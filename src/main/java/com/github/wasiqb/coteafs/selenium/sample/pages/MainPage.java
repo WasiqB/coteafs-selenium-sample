56ffcedfdcf2dbc367d46b6016955ea4bff8b0da
WasiqB <wasbhamla2005@gmail.com>
2019-08-08 21:47:14 +0530

    Udated for v3.0-beta1

--- src/main/java/com/github/wasiqb/coteafs/selenium/sample/pages/MainPage.java
+++ src/main/java/com/github/wasiqb/coteafs/selenium/sample/pages/MainPage.java
@@ -1,33 +1,33 @@
 package com.github.wasiqb.coteafs.selenium.sample.pages;
 
-import com.github.wasiqb.coteafs.selenium.core.BrowserPage;
-import com.github.wasiqb.coteafs.selenium.core.element.IElementActions;
-import com.github.wasiqb.coteafs.selenium.core.element.IMouseActions;
-import com.github.wasiqb.coteafs.selenium.core.enums.WaitStrategy;
-import org.openqa.selenium.By;
-import org.testng.Assert;
+import static java.text.MessageFormat.format;
 
 import java.util.Optional;
 
-import static java.text.MessageFormat.format;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+
+import com.github.wasiqb.coteafs.selenium.core.BrowserPage;
+import com.github.wasiqb.coteafs.selenium.core.element.IMouseActions;
+import com.github.wasiqb.coteafs.selenium.core.enums.WaitStrategy;
 
 public class MainPage extends BrowserPage {
-	IElementActions page () {
-		return onElement (By.id ("page"), WaitStrategy.VISIBLE);
-	}
-
-	private IElementActions nav () {
-		return page ().find (By.tagName ("nav"));
-	}
-
-	public IMouseActions nav (String name) {
-		Optional <IElementActions> menu = nav ().finds (By.cssSelector ("div > a"))
+	public IMouseActions nav (final String name) {
+		final Optional <IMouseActions> menu = nav ().finds (By.cssSelector ("div > a"))
 			.stream ()
 			.filter (m -> m.text ()
 				.equalsIgnoreCase (name))
 			.findFirst ();
-		if (menu.isPresent ()) return (IMouseActions) menu.get ();
+		if (menu.isPresent ()) return menu.get ();
 		Assert.fail (format ("Menu not found: {0}.", name));
 		return null;
+	}
+
+	IMouseActions page () {
+		return onClickable (By.id ("page"), WaitStrategy.VISIBLE);
+	}
+
+	private IMouseActions nav () {
+		return page ().find (By.tagName ("nav"));
 	}
 }
